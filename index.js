3f2b3bffc1addbdc13106b6b4da20c5e500862d8
xamgore <xamgore@ya.ru>
2017-04-07 13:06:41 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+require('babel-register') //({ stage: 0 })
+// require('babel/polyfill')
+
+// require('babel-core/register')({
+//     presets: ["es2015"],
+//     plugins: ["syntax-async-functions", "syntax-async-generators"]
+// })
+
+require('./app')
