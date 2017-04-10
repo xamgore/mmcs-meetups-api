528da55c3da180a3931304c638d2053c14b8f311
xamgore <xamgore@ya.ru>
2017-04-10 11:11:41 +0300

    Set correct CORS headers

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import Koa from 'koa'
+import Cors from 'kcors'
 import Json from 'koa-json'
 import Logger from 'koa-logger'
 import BodyParser from 'koa-bodyparser'
 
 const koa = new Koa()
 koa.use(Logger())
+koa.use(Cors())
 koa.use(BodyParser())
 koa.use(Json({ pretty: false, param: 'pretty', spaces: 2 }))
 
