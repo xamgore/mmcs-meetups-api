f3168a8d03c98a8c4c711844e88699abe6922d77
xamgore <xamgore@ya.ru>
2017-05-26 17:08:35 +0300

    Remove .json extension from urls

--- app.js
+++ app.js
@@ -3,16 +3,20 @@ import Cors from 'kcors'
 import Etag from 'koa-etag'
 import Json from 'koa-json'
 import Logger from 'koa-logger'
+import Rewrite from 'koa-rewrite'
 import BodyParser from 'koa-bodyparser'
 import Conditional from 'koa-conditional-get'
 
 const koa = new Koa()
+
 koa.use(Logger())
 koa.use(Cors())
 koa.use(BodyParser())
 koa.use(Json({ pretty: false, param: 'pretty', spaces: 2 }))
 koa.use(Conditional())
 koa.use(Etag())
+
+koa.use(Rewrite(/^(.*)\.json/, '$1'))
 
 import api17 from './api/v17'
 
