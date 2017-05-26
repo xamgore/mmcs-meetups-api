e4019f9a7e7508136e6746e684e8fcafbc77438c
xamgore <xamgore@ya.ru>
2017-05-26 16:11:02 +0300

    Add etag-labels to reduce network usage

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 import Koa from 'koa'
 import Cors from 'kcors'
+import Etag from 'koa-etag'
 import Json from 'koa-json'
 import Logger from 'koa-logger'
 import BodyParser from 'koa-bodyparser'
+import Conditional from 'koa-conditional-get'
 
 const koa = new Koa()
 koa.use(Logger())
 koa.use(Cors())
 koa.use(BodyParser())
 koa.use(Json({ pretty: false, param: 'pretty', spaces: 2 }))
+koa.use(Conditional())
+koa.use(Etag())
 
 import api17 from './api/v17'
 
